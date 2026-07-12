d5370a63b32299647e0f47a47e9f1b258c98e83a
rakeshyadhav21 <rakeshyadhav329@gmail.com>
2026-07-13 01:55:05 +0530

    UI done after educator components

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import AddCourse from './pages/educator/AddCourse'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
 
@@ -32,7 +33,7 @@ const App = () => {
         
         {/* Educator Routes */}
         <Route path='/educator' element={<Educator/>}>
-          <Route path='educator' element={<Dashboard/>}/>
+          <Route path='/educator' element={<Dashboard/>}/>
           <Route path='add-course' element={<AddCourse/>}/>
           <Route path='my-courses' element={<MyCourses/>}/>
           <Route path='students-enrolled' element={<StudentsEnrolled/>}/>
