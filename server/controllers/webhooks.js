fa7ebb77165a776af2850a0d37ddee743a646e90
rakeshyadhav21 <rakeshyadhav329@gmail.com>
2026-08-12 00:22:11 +0530

    DB Connection error resolved

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -21,7 +21,7 @@ export const clerkWebhooks = async (req, res) => {
           _id: data.id,
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imageUrl: data.image_url,
+          imageURL: data.image_url,
         };
 
         await User.create(userData);
@@ -33,7 +33,7 @@ export const clerkWebhooks = async (req, res) => {
         const userData = {
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imageUrl: data.image_url,
+          imageURL: data.image_url,
         };
 
         await User.findByIdAndUpdate(data.id, userData);
--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
